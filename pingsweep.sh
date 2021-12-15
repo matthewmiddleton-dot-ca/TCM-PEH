f092a1f981e452793f40401bae573133f1f0eaa2
Matt Middleton <middleton.m@gmail.com>
2021-12-15 18:09:05 -0500

    Minor input check added
    The input check only gets triggered on an empty argument, not on an invalid argument.

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+if [ "$1" == "" ] #Checks whether or not anything was entered as an argument
+then
+echo "You forgot an IP address!"
+echo "Syntax: ./pingsweep.sh 192.168.4"
+
+else
 for ip in `seq 1 254`; do #Allows only last octet to be swept
 
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" & #Takes first 3 octets as input, trims non-IP data
+done
+fi
