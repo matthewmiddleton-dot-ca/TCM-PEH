5fb926d0110f6f49c75f74e2789cba355a1a4d88
Matt Middleton <middleton.m@gmail.com>
2021-12-16 08:04:42 -0500

    Changed argument check to IP-focus
    The previous check of the argument passed to the script focused on the case where a user doesn't provide input. There are lots of possible entry options that wouldn't be valid, so the logic has been reversed; it now checks if the entry is 3 numeric values separated by dots, and rejects anything else.
    
    The regex is a relatively simple one, so it doesn't check for values outside the 0-255 range (or 1-254 range for the first octet).

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-if [ "$1" == "" ] #Checks whether or not anything was entered as an argument
+if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] # Changed error handling so it only accepts a Class C IP, although the regex doesn't limit each octet to 255
 then
-echo "You forgot an IP address!"
-echo "Syntax: ./pingsweep.sh 192.168.4"
 
-else
 for ip in `seq 1 254`; do #Allows only last octet to be swept
 
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" & #Takes first 3 octets as input, trims non-IP data
 done
+
+else
+echo "Invalid argument passed to pingsweep"
+echo "Syntax: pingsweep.sh 192.168.4"
+
 fi
